473c1ff563b7fb36e7c0222db6e2e24d9de6007e
Krystian Sniegoski <krystian.sniegoski@onet.eu>
2024-01-19 12:40:01 +0100

    Dodanie objetości walca

--- plik.c
+++ plik.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
-float calculate_volume(float length, float width, float height) {
+#define PI 3.14159
+
+float calculate_volume_prism(float length, float width, float height) {
     return length * width * height;
 }
 
+float calculate_volume_cylinder(float radius, float height) {
+    return PI * pow(radius, 2) * height;
+}
+
 int main() {
-    float length, width, height;
+    int choice;
+    float length, width, height, radius;
 
-    printf("Podaj dlugosc prostopadloscianu: ");
-    scanf("%f", &length);
+    printf("Menu:\n");
+    printf("1. Objêtosc prostopad³oscianu\n");
+    printf("2. Objetosc walca\n");
+    printf("Wybierz opcje (1-2): ");
+    scanf("%d", &choice);
 
-    printf("Podaj szerokosc prostopadloscianu: ");
-    scanf("%f", &width);
+    switch (choice) {
+        case 1:
+            printf("Podaj dlugosc prostopadloscianu: ");
+            scanf("%f", &length);
+            printf("Podaj szerokosc prostopad³oscianu: ");
+            scanf("%f", &width);
+            printf("Podaj wysokosc prostopadloscianu: ");
+            scanf("%f", &height);
+            printf("Objetosc prostopadloscianu wynosi: %.2f\n", calculate_volume_prism(length, width, height));
+            break;
 
-    printf("Podaj wysokosc prostopadloscianu: ");
-    scanf("%f", &height);
+        case 2:
+            printf("Podaj promien walca: ");
+            scanf("%f", &radius);
+            printf("Podaj wysokosc walca: ");
+            scanf("%f", &height);
+            printf("Objetoœæ walca wynosi: %.2f\n", calculate_volume_cylinder(radius, height));
+            break;
 
-    float volume = calculate_volume(length, width, height);
-
-    printf("Objetosc prostopadloscianu wynosi: %.2f\n", volume);
+        default:
+            printf("Niepoprawny wybor. Wyjscie z programu.\n");
+            return 1;
+    }
 
     return 0;
 }
